ab7262cd2fe8bf9f446f71df31719315ce3fc08e
Daniel Tsui <tsui.daniel@gmail.com>
2017-10-17 16:55:18 +1100

    ajax & api

--- 08-ajax/food_trucking/config/routes.rb
+++ 08-ajax/food_trucking/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :likes
   resources :dishes
   resources :users
+
+  get '/api/dishes', to: 'api/dishes#index'
+  post '/api/likes', to: 'api/likes#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
