a26210b5f357aa042f07e9645f7b7a28b5ae52dc
Daniel Tsui <tsui.daniel@gmail.com>
2017-11-02 16:04:16 +1100

    opps bugs

--- 10-nodejs/timer/app.js
+++ 10-nodejs/timer/app.js
@@ -9,26 +9,28 @@ var timer = {
   },
   init: function() {
     this.elems.displayH1.textContent = this.seconds
-    this.elems.startButton.addEventListener('click', this.startHandler)
-    this.elems.stopButton.addEventListener('click', this.stopHandler)
-    this.elems.resetButton.addEventListener('click', this.resetHandler)
+    this.elems.startButton.addEventListener('click', this.startHander.bind(this))
+    this.elems.stopButton.addEventListener('click', this.stopHander.bind(this))
+    this.elems.resetButton.addEventListener('click', this.resetHander.bind(this))
   },
 
-  startHandler: () => {
+  startHander() {
     this.timerId = setInterval(() => {
       this.seconds++ // this is now the timer oject
       this.elems.displayH1.textContent = this.seconds
     }, 1000)
   }, 
 
-  stopHandler: () => {
+  stopHander() {
     clearInterval(this.timerId);
   },
 
-  resetHandler: () => {
+  resetHander() {
     this.seconds = 0;
     this.elems.displayH1.textContent = this.seconds
   }
 }
 
 timer.init();
+
+
