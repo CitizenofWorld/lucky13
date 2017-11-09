ff06661600756353cf5164b5e09276441eac86a4
Daniel Tsui <tsui.daniel@gmail.com>
2017-11-09 16:09:07 +1100

    tweetbox

---
+++ 11-react/tweetbox_demo/app/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Tweetbox from './Tweetbox'
+import BarSlider from './BarSlider'
+
+export default function App() {
+  return <div>
+    <h1>tweetbox demo</h1>
+    <Tweetbox />
+  </div>
+}
